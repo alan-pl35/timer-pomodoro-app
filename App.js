c21f2f4a48cba8316b67576ff93386db607a71a5
alan-pl35 <163952926+alan-pl35@users.noreply.github.com>
2024-05-03 20:55:56 -0600

    styles was added in main component

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View, Platform , TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View
+        style={{
+          borderWidth: 6,
+          borderColor: "black",
+          flex: 1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 30,
+        }}
+      >
+        <Text style={styles.texto}>Hi Mom</Text>
+        <StatusBar style="auto" />
+        <TouchableOpacity  style={styles.button} >
+          <Text style={{color: 'white' , fontWeight: 'bold'}}>Press here</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  texto: { fontSize: 32, color: "crimson" },
+  otroTexto: { fontSize: 32, color: "gray" },
+  button: {
+    backgroundColor: '#333333',
+    marginTop: 15,
+    borderRadius: 15,
+    padding: 15,
+    alignItems: 'center'
+  }
 });
